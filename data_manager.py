c2e1dc5c463c1a66df3f3789640748898a9a16d0
PHouthone <123578358+Phouthone18y@users.noreply.github.com>
2024-12-16 13:55:39 +0700

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 
+
 class DataManager:
     def __init__(self):
         self.entries = []
@@ -9,11 +10,13 @@ class DataManager:
         self.data_file = "account_data.json"
         self.load_data()
 
+
     def add_entry(self, date, amount, note):
         date_str = date.strftime("%Y-%m-%d")
         self.entries.append((date_str, amount, note))
         self.save_data()
         self.notify_observers()
+
 
     def delete_entry(self, date, amount, note):
         entry_to_remove = (date, amount, note)
@@ -22,9 +25,21 @@ class DataManager:
             self.save_data()
             self.notify_observers()
 
+
+    def update_entry(self, old_date_str, old_amount, old_note, new_date, new_amount, new_note):
+        new_date_str = new_date.strftime("%Y-%m-%d")
+        for i, (date_str, amount, note) in enumerate(self.entries):
+            if date_str == old_date_str and amount == old_amount and note == old_note:
+               self.entries[i] = (new_date_str, new_amount, new_note)
+               self.save_data()
+               self.notify_observers()
+               return
+
+
     def filter_data(self, month):
         self.current_month = month
         self.notify_observers()
+
 
     def get_filtered_data(self):
         if self.current_month == "ທັງໝົດ":
@@ -39,9 +54,11 @@ class DataManager:
                 print(f"ຮູບແບບວັນທີບໍ່ຖືກຕ້ອງ: {date_str}")
         return filtered
 
+
     def notify_observers(self):
         for observer in self.observers:
             observer()
+
 
     def save_data(self):
         new_entries = []
@@ -53,6 +70,7 @@ class DataManager:
                 print(f"ຈຳນວນເງິນບໍ່ຖືກຕ້ອງສຳລັບການບັນທຶກ: {amount}")
         with open(self.data_file, 'w') as f:
             json.dump(new_entries, f, indent=4)
+
 
     def load_data(self):
         try:
